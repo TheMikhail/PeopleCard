5c90ad135291771f35636dafdc6aece44ff1c7e6
TheMikhail <74441413+TheMikhail@users.noreply.github.com>
2025-08-05 09:49:24 +0700

    working with applications by clicking on text fields

--- app/src/main/java/com/example/peoplecard/network/PeopleServices.kt
+++ app/src/main/java/com/example/peoplecard/network/PeopleServices.kt
@@ -1,18 +1,27 @@
 package com.example.peoplecard.network
 
 import com.example.peoplecard.model.PeopleResponse
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
+var certPinner = CertificatePinner.Builder()
+    .add(
+        "appmattus.com",
+        "sha256/4hw5tz+scE+TW+mlai5YipDfFWn1dqvfLG+nU7tq1V8="
+    )
+    .build()
 val client = OkHttpClient.Builder()
+    .certificatePinner(certPinner)
     .build()
 val retrofit = Retrofit.Builder()
         .baseUrl("https://randomuser.me/")
-        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 
     interface RetrofitServices {
